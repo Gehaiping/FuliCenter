9344ec0c0c274a32a817f0c670089edba7edc80a
gehaiping <804563299@qq.com>
2017-01-16 19:11:31 +0800

    用户登录及逻辑判断

--- app/src/main/java/cn/ucai/fulicenter/controller/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/LoginActivity.java
@@ -1,15 +1,33 @@
 package cn.ucai.fulicenter.controller.activity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.model.bean.Result;
+import cn.ucai.fulicenter.model.net.IModelUser;
+import cn.ucai.fulicenter.model.net.ModelUser;
+import cn.ucai.fulicenter.model.net.OnCompletListener;
+import cn.ucai.fulicenter.model.util.CommonUtils;
+import cn.ucai.fulicenter.model.util.ResultUtils;
 import cn.ucai.fulicenter.view.MFGT;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.etUserName)
+    EditText etUserName;
+    @BindView(R.id.etPassword)
+    EditText etPassword;
+
+    IModelUser model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +44,64 @@ public class LoginActivity extends AppCompatActivity {
                 MFGT.finish(this);
                 break;
             case R.id.btnLogin:
-
+                checkInput();
                 break;
             case R.id.btnRegister:
                 MFGT.gotoRegister(this);
                 break;
         }
+    }
+
+    private void checkInput() {
+        String userName = etUserName.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(userName)) {
+            etUserName.setError(getString(R.string.user_name_connot_be_empty));
+            etUserName.requestFocus();
+        } else if (TextUtils.isEmpty(password)) {
+            etPassword.setError(getString(R.string.password_connot_be_empty));
+            etPassword.requestFocus();
+        } else {
+            login(userName, password);
+        }
+    }
+
+    private void login(String userName, String password) {
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setMessage(getString(R.string.logining));
+
+        model = new ModelUser();
+        model.login(this, userName, password, new OnCompletListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                if (s != null) {
+                    Result result = ResultUtils.getResultFromJson(s, Result.class);
+                    if (result != null) {
+                        if (result.isRetMsg()) {
+                            CommonUtils.showShortToast("登录成功");
+                            MFGT.finish(LoginActivity.this);
+                        } else {
+                            if (result.getRetCode() == I.MSG_LOGIN_UNKNOW_USER) {
+                                CommonUtils.showShortToast(getString(R.string.login_fail_unknow_user));
+                            }
+                            if (result.getRetCode() == I.MSG_LOGIN_ERROR_PASSWORD) {
+                                CommonUtils.showShortToast(getString(R.string.login_fail_error_password));
+                            }
+                        }
+                    } else {
+                        CommonUtils.showShortToast(getString(R.string.login_fail));
+                    }
+                } else {
+                    CommonUtils.showShortToast(getString(R.string.login_fail));
+                }
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onError(String error) {
+                dialog.dismiss();
+                CommonUtils.showShortToast(error);
+            }
+        });
     }
 }
